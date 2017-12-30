89fdaa5792c5b2c31ab09e09824af3874df81b1f
dick3741 <233955487@qq.com>
2017-12-30 17:12:25 +0800

    价格过滤字符串转数字

--- README.md
+++ README.md
@@ -5,9 +5,9 @@
 [Preview](http://www.pushishuang.cn:3000/#/)
 
 下载代码后执行：
-- `cnpm install`
-- `node server/bin/www`
-- `cnpm run dev`
+1. `cnpm install`
+2. 在保证mongodb服务启动的情况下执行 `node server/bin/www` ，关于mongodb的配置，参考[我的blog](http://blog.csdn.net/dick3741/article/details/78244615)
+3. `npm run dev` ，就可以打开网站实例了
 
 >环境参数：
-Vue.js 2.x、Vue Router 2.x、Axios 0.16、Vuex 2.x、Express 4.x、MongoDB
+>Vue.js 2.x、Vue Router 2.x、Axios 0.16、Vuex 2.x、Express 4.x、MongoDB
--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -133,20 +133,20 @@
             mdShowCart:false,
             priceFilter:[
               {
-                startPrice:"0.00",
-                endPrice:"100.00"
+                startPrice:0.00,
+                endPrice:100.00
               },
               {
-                startPrice:"100.00",
-                endPrice:"500.00"
+                startPrice:100.00,
+                endPrice:500.00
               },
               {
-                startPrice:"500.00",
-                endPrice:"1000.00"
+                startPrice:500.00,
+                endPrice:1000.00
               },
               {
-                startPrice:"1000.00",
-                endPrice:"5000.00"
+                startPrice:1000.00,
+                endPrice:5000.00
               }
             ],
             priceChecked:'all',
